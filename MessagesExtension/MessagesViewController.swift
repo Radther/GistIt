22bb396e04340076e6753f2ac0bb912925437d84
Tom Singleton <tomquil13@gmail.com>
2016-10-14 19:06:11 +0100

    Finished!

--- MessagesExtension/MessagesViewController.swift
+++ MessagesExtension/MessagesViewController.swift
@@ -13,21 +13,117 @@ class MessagesViewController: MSMessagesAppViewController {
     
     @IBOutlet var fileNameLabel: UITextField!
     @IBOutlet var codeTextView: UITextView!
+    @IBOutlet var gistButton: UIButton!
+    var task: URLSessionDataTask?
     @IBAction func gistIt(_ sender: AnyObject) {
-        
+        guard let fileName = fileNameLabel.text, fileName.characters.count > 0 else {
+            return
+        }
+        guard let code = codeTextView.text, code.characters.count > 0 else {
+            return
+        }
+        let url = URL(string: "https://api.github.com/gists")!
+        let json: JSON = [
+            "description" : "GistIt Gist",
+            "public" : false,
+            "files" : [
+                fileName: [
+                    "content" : code
+                ]
+            ]
+        ]
+        print(json.debugDescription)
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        print(request)
+        guard let jsonData = try? json.rawData() else {
+            return
+        }
+        request.httpBody = jsonData
+        gistButton.isEnabled = false
+        print("Disabled the button")
+        gistButton.titleLabel?.text = "Gisting..."
+        print("Gisting...")
+        let session = URLSession.shared
+        task = session.dataTask(with: request, completionHandler: { (data, response, error) in
+            print("Task Complete")
+            guard error == nil else {
+                DispatchQueue.main.async {
+                    self.gistButton.titleLabel?.text = "Failed :/ Tap to try again"
+                    self.gistButton.isEnabled = true
+                }
+                print("Errored")
+                return
+            }
+            
+            guard let data = data else {
+                DispatchQueue.main.async {
+                    self.gistButton.titleLabel?.text = "Failed :/ Tap to try again"
+                    self.gistButton.isEnabled = true
+                }
+                print("Errored")
+                return
+            }
+            
+            let responseJson = JSON(data: data)
+            guard let gistUrlString = responseJson["html_url"].string,
+            let gistUrl = URL(string: gistUrlString) else {
+                DispatchQueue.main.async {
+                    self.gistButton.titleLabel?.text = "Failed :/ Tap to try again"
+                    self.gistButton.isEnabled = true
+                }
+                print("Errored")
+                return
+            }
+            DispatchQueue.main.async {
+                self.conv?.insertText(gistUrl.absoluteString, completionHandler: { (error) in
+                    if let error = error {
+                        print(error)
+                    }
+                })
+                print("Success!")
+                self.gistButton.titleLabel?.text = "Gist It"
+                self.gistButton.isEnabled = true
+                self.requestPresentationStyle(.compact)
+            }
+        })
+        print(task)
+        task?.resume()
     }
     
     var conv: MSConversation?
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-//        let tapGestureRecogniser = UITapGestureRecognizer(target: self, action: #selector(expandExtension))
+        let tapGestureRecogniser = UITapGestureRecognizer(target: self, action: #selector(dismissKeyboard))
+        view.addGestureRecognizer(tapGestureRecogniser)
 //        fileNameLabel.addGestureRecognizer(tapGestureRecogniser)
 //        codeTextView.addGestureRecognizer(tapGestureRecogniser)
+        setupKeyboardNotifications()
     }
     
-    func expandExtension() {
-        requestPresentationStyle(.expanded)
+    func dismissKeyboard() {
+        view.endEditing(true)
+    }
+    
+    func setupKeyboardNotifications() {
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown), name: NSNotification.Name.UIKeyboardDidShow, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
+    }
+    
+    func keyboardWasShown(aNotification:NSNotification) {
+        let info = aNotification.userInfo
+        let infoNSValue = info![UIKeyboardFrameBeginUserInfoKey] as! NSValue
+        let kbSize = infoNSValue.cgRectValue.size
+        let contentInsets = UIEdgeInsetsMake(0.0, 0.0, kbSize.height, 0.0)
+        codeTextView.contentInset = contentInsets
+        codeTextView.scrollIndicatorInsets = contentInsets
+    }
+    
+    func keyboardWillBeHidden(aNotification:NSNotification) {
+        let contentInsets = UIEdgeInsets.zero
+        codeTextView.contentInset = contentInsets
+        codeTextView.scrollIndicatorInsets = contentInsets
     }
     
     override func didReceiveMemoryWarning() {
